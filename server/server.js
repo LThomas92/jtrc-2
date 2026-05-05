e37d3b51dfa6fb903b8170711d09572406682ec9
Lawrence Thomas <lawrencegthomas@gmail.com>
2026-05-05 00:45:03 -0400

    Update CORS allowed origins

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ const allowedOrigins = [
   "http://localhost:5174",
   "https://jtsrusticcuisine.com",
   "https://www.jtsrusticcuisine.com",
-  "https://jtrc-2-client-izfplytoq-lthomas92s-projects.vercel.app"
+  "https://jtrc-2-client-izfplytoq-lthomas92s-projects.vercel.app",
+   "https://jtrc-2-client.vercel.app"
 ];
 
 const corsOptions = {
